7c27a3a8592c0f976aa30566901f17d60730d1af
catdintran <trancatdin86@yahoo.com>
2017-10-19 17:50:56 +0000

    Update JavaCodes.java

--- JavaCodes.java
+++ JavaCodes.java
@@ -16,3 +16,18 @@ Arrays.stream("abcdeabcdabcaba".split("")).parallel()
 									.append("\' with Frequencies: ")
 									.append(e.getValue())));
 			
+
+// in case we have to manually serve static file to html, do this:
+<link href="/discover/resources/synonym.css" rel="stylesheet">
+<script src="/discover/resources/jqueryLib.js" ></script>
+<script src="/discover/resources/synonymFunctions.js" ></script>
+
+@RequestMapping(value = "/resources/{file}", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public byte[] getStaticFile(@PathVariable("file") String fileName) throws IOException {				 
+		String path = System.getProperty("user.dir").replace("/bin","") + "/webapps/discover/resources/" + fileName;
+		InputStream in = new FileInputStream(new File(path));
+		return IOUtils.toByteArray(in);	
+	}
+  
+	
