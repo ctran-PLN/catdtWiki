f8e72c1085687f200a825f3f7a997456bb2dce20
catdintran <trancatdin86@yahoo.com>
2018-01-24 10:50:06 -0500

    Update pythonCodes.py

--- pythonCodes.py
+++ pythonCodes.py
@@ -31,3 +31,24 @@ monums={'JAN':'1', 'FEB':'2', 'MAR':'3', 'APR':'4', 'MAY':'5', 'JUN':'6', 'JUL':
 #month
 monos=monthStr.map(monums)
 
+# try_except_wrapper
+import json
+def try_except_wrapper(f,*args, **kargs):
+  try:
+    f(*args)
+  except Exception as err:
+    print(err)
+
+def one_param(x):
+  print('One param: ' + str(x))
+def two_param(x,y):
+  print('Two param: ' + str(x/y) )
+def key_param(x):
+  print('Key param : ' + json.dumps(x) )
+    
+a=[1,2,0,3,4,5]
+for i in a:
+  try_except_wrapper(one_param, i)
+  try_except_wrapper(two_param, 5, i)
+  try_except_wrapper(key_param, {5:i})
+# try_except_wrapper
