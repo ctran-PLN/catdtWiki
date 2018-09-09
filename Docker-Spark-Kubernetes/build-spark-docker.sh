22afb1c9d39f4e57ad204bf0ea8a86fc38845595
catdintran <trancatdin86@yahoo.com>
2018-09-09 15:30:16 -0400

    Update build-spark-docker.sh

--- Docker-Spark-Kubernetes/build-spark-docker.sh
+++ Docker-Spark-Kubernetes/build-spark-docker.sh
@@ -1,7 +1,7 @@
 #wget http://apache.claz.org/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
 #tar xvf spark-2.3.1-bin-hadoop2.7.tgz
 cd spark-2.3.1-bin-hadoop2.7
-docker build -t catdt/spark:2.3 -f kubernetes/dockerfiles/spark/Dockerfile .
+docker build -t catdt-spark -f kubernetes/dockerfiles/spark/Dockerfile .
 
 docker tag catdt-spark catdintran/catdt/spark:firsttry
 docker push catdintran/catdt/spark
