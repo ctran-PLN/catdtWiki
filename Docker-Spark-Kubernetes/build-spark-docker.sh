e01bedb704048af47658c61722979bf23191ce66
catdintran <trancatdin86@yahoo.com>
2018-09-09 15:36:39 -0400

    Update build-spark-docker.sh

--- Docker-Spark-Kubernetes/build-spark-docker.sh
+++ Docker-Spark-Kubernetes/build-spark-docker.sh
@@ -1,5 +1,5 @@
-#wget http://apache.claz.org/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
-#tar xvf spark-2.3.1-bin-hadoop2.7.tgz
+wget http://apache.claz.org/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
+tar xvf spark-2.3.1-bin-hadoop2.7.tgz
 cd spark-2.3.1-bin-hadoop2.7
 docker build -t catdt-spark:2.3 -f kubernetes/dockerfiles/spark/Dockerfile .
 
