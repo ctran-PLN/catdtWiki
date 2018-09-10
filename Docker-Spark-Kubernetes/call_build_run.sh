62ac67b108f029a67be6cd7d132b6b259344e037
catdintran <trancatdin86@yahoo.com>
2018-09-10 07:06:36 -0400

    Update call_build_run.sh

--- Docker-Spark-Kubernetes/call_build_run.sh
+++ Docker-Spark-Kubernetes/call_build_run.sh
@@ -4,4 +4,4 @@ docker build -t catdt/docker-spark:1.0 .
 # call docker images to check built images
 
 # run
-docker run --name spark -it catdt/docker-spark:1.0
+docker run -p 8888:8888 --name spark -it catdt/docker-spark:1.0
