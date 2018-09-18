f31bd1035f84e419fe0e972214395011a114df92
catdintran <trancatdin86@yahoo.com>
2018-09-17 21:18:21 -0400

    Create spark-context.sh

---
+++ Docker-Spark-Kubernetes/spark-context.sh
@@ -0,0 +1,6 @@
+# create a new context using our current context as a base
+CURRENT_CONTEXT=$(kubectl config view -o jsonpath='{.current-context}')
+USER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.user}')
+CLUSTER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.cluster}')
+kubectl config set-context spark --namespace=spark-cluster --cluster=${CLUSTER_NAME} --user=${USER_NAME}
+kubectl config use-context spark
