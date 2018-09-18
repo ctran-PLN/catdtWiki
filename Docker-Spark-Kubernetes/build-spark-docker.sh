e662858196c9e7873b8a0e4e3ecb55bbb92169ab
catdintran <trancatdin86@yahoo.com>
2018-09-17 21:10:09 -0400

    Update build-spark-docker.sh

--- Docker-Spark-Kubernetes/build-spark-docker.sh
+++ Docker-Spark-Kubernetes/build-spark-docker.sh
@@ -3,6 +3,12 @@ tar xvf spark-2.3.1-bin-hadoop2.7.tgz
 cd spark-2.3.1-bin-hadoop2.7
 docker build -t catdt-spark:2.3 -f kubernetes/dockerfiles/spark/Dockerfile .
 
-#docker tag catdt-spark catdintran/catdt/spark:firsttry
-#docker push catdintran/catdt/spark
+#create namespace
+kubectl create -f catdtWiki/Docker-Spark-Kubernetes/namespace-spark-cluster.yaml
 
+# create a new context using our current context as a base
+CURRENT_CONTEXT=$(kubectl config view -o jsonpath='{.current-context}')
+USER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.user}')
+CLUSTER_NAME=$(kubectl config view -o jsonpath='{.contexts[?(@.name == "'"${CURRENT_CONTEXT}"'")].context.cluster}')
+kubectl config set-context spark --namespace=spark-cluster --cluster=${CLUSTER_NAME} --user=${USER_NAME}
+kubectl config use-context spark
