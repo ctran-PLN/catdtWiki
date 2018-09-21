b25e25a97b761f3da2c8aeb9ae94900ddab0e1ee
catdintran <trancatdin86@yahoo.com>
2018-09-20 20:22:22 -0400

    Update build-spark-docker.sh

--- Docker-Spark-Kubernetes/build-spark-docker.sh
+++ Docker-Spark-Kubernetes/build-spark-docker.sh
@@ -1,4 +1,17 @@
+
+sudo apt-get -y update && \
+    sudo apt-get install maven -y && \
+    sudo apt-get install --no-install-recommends -y openjdk-8-jre-headless ca-certificates-java && \
+    sudo apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
 wget http://apache.claz.org/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz
 tar xvf spark-2.3.1-bin-hadoop2.7.tgz
 cd spark-2.3.1-bin-hadoop2.7
-docker build -t catdt-spark:2.3 -f kubernetes/dockerfiles/spark/Dockerfile .
+spark_path=aa=$(echo pwd)
+echo 'export SPARK_HOME=$spark_path' >> ~/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre/' >> ~/.bashrc
+echo 'export PATH=$PATH:$JAVA_HOME' >> ~/.bashrc
+
+source ~/.bashrc
+
