98bcfa08bf786f44ad9fb48fc149d7f938e92445
catdintran <trancatdin86@yahoo.com>
2018-09-25 16:42:00 -0400

    Update install-kubernetes.sh

--- Docker-Spark-Kubernetes/install-kubernetes.sh
+++ Docker-Spark-Kubernetes/install-kubernetes.sh
@@ -1,3 +1,18 @@
+# CentOs
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubelet kubeadm kubectl
+
+
+#############
 sudo apt-get update && sudo apt-get install -y apt-transport-https
 
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
